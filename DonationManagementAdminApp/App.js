ff9dd30792b75b5f15e46cd55e595233c61bbded
L.A.M.D. Perera <mokshikaperera98@gmail.com>
2022-12-24 19:34:24 +0530

    done

--- DonationManagementAdminApp/App.js
+++ DonationManagementAdminApp/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import { Dashboard, Login, PaymentDashboard, Register, Splash,EventDashboard, CompleteEvent, Event, EventManagerPayments, OrgPayments, DonorDetails, OrgPaymentDetails, DonationDetailsList} from "./screens/";
+import { Dashboard, Login, Register, Splash } from "./screens/";
 
 const Stack = createStackNavigator();
 
